b3fbdb56416a0cd90539bf52e4c24848da9a44f5
Ahmed elsayed <87664326+Ahmedelsayed265@users.noreply.github.com>
2023-03-02 01:24:06 +0200

    1

--- src/components/HomepageHeader.js
+++ src/components/HomepageHeader.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import HeaderSlider from "./HeaderSlider";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 const HomepageHeader = () => {
+  const [formData, setFormData] = useState({});
+  let headersList = {
+    Accept: "/",
+    "Content-Type": "multipart/form-data"
+  };
+  let requestOptions = {
+    url: "", //home top slider post api
+    method: "POST",
+    headers: headersList,
+    data: formData
+  };
+  const handleSubmitNew = e => {
+    e.preventDefault();
+    axios
+      .request(requestOptions)
+      .then(() => {
+        document.getElementById("addNewForm").reset();
+        toast.success("تمت الاضافه بنجاح");
+      })
+      .catch(() => {
+        document.getElementById("addNewForm").reset();
+        toast.error("عذرا حدث خطا حاول مره اخرى");
+      });
+  };
   return (
     <main>
       <div className="wrapper">
@@ -13,7 +39,11 @@ const HomepageHeader = () => {
         </div>
         <div className="add-new">
           <h2>اضافه عنصر جديد</h2>
-          <form action="" className="input-box hero-img">
+          <form
+            onSubmit={handleSubmitNew}
+            id="addNewForm"
+            className="input-box hero-img"
+          >
             <div className="file_feild">
               <p>حمل صوره جديده</p>
               <input
@@ -22,13 +52,18 @@ const HomepageHeader = () => {
                 name="file"
                 className="file_in"
                 accept="image/*"
+                onChange={e => {
+                  setFormData({ ...formData, image: e.target.files[0] });
+                }}
               />
             </div>
             <textarea
-              placeholder="اضافه نص"
+              required
+              rows="2"
               name="hero-text"
               id="hero-text"
-              rows="2"
+              placeholder="اضافه نص"
+              onChange={e => setFormData({ ...formData, text: e.target.value })}
             />
             <button type="submit" className="btn">
               اضافه
