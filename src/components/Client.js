72507755504bf45a0cc97e701b7b8fafb6e8f44f
Ahmed elsayed <87664326+Ahmedelsayed265@users.noreply.github.com>
2023-03-12 02:50:17 +0200

    1

--- src/components/Client.js
+++ src/components/Client.js
@@ -1,7 +1,45 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-const Client = ({ client }) => {
+const Client = ({ client, id }) => {
   const [edit, setEdit] = useState(false);
+  const [formData, setFormData] = useState({});
+  const handleEdit = () => {
+    setEdit(true);
+  };
+  const handleSubmitEdits = e => {
+    e.preventDefault();
+    setEdit(false);
+    const reader = new FileReader();
+    reader.readAsDataURL(formData.image);
+    reader.onload = () => {
+      const imageDataUrl = reader.result;
+      axios
+        .patch(`https://test.dummydealer.com/api/v1/admin/slider/${id}`, {
+          image: imageDataUrl,
+          name: formData.name
+        })
+        .then(() => {
+          toast.success("تم التعديل بنجاح");
+        })
+        .catch(() => {
+          toast.error("اختار صوره اخرى بحجم اقل");
+        });
+    };
+  };
+  const handleDelete = async () => {
+    axios
+      .delete(`https://test.dummydealer.com/api/v1/admin/slider/${id}`)
+      .then(() => {
+        document.getElementById("uform").reset();
+        toast.success("تم الحذف بنجاح");
+      })
+      .catch(() => {
+        document.getElementById("uform").reset();
+        toast.error("عذرا حدث خطا حاول مره اخرى");
+      });
+  };
   return (
     <div className="img">
       <div className="container">
@@ -17,18 +55,19 @@ const Client = ({ client }) => {
           </p>
         </div>
         <div className="control-btn">
-          <button
-            className="edit"
-            onClick={() => {
-              setEdit(true);
-            }}
-          >
+          <button className="edit" onClick={handleEdit}>
             تعديل
           </button>
-          <button className="delete">حذف</button>
+          <button onClick={handleDelete} className="delete">
+            حذف
+          </button>
         </div>
       </div>
-      <form action="" className={edit ? "active" : ""}>
+      <form
+        onSubmit={handleSubmitEdits}
+        id="uform"
+        className={edit ? "active" : ""}
+      >
         <div className="file_feild">
           <p>حمل صوره جديده</p>
           <input
@@ -37,6 +76,9 @@ const Client = ({ client }) => {
             name="file"
             className="file_in"
             accept="image/*"
+            onChange={e => {
+              setFormData({ ...formData, image: e.target.files[0] });
+            }}
           />
         </div>
         <textarea
@@ -44,15 +86,13 @@ const Client = ({ client }) => {
           name="hero-text"
           id="hero-text"
           rows="2"
+          onChange={e =>
+            setFormData({
+              ...formData,
+              name: e.target.value
+            })}
         />
-        <button
-          type="submit"
-          className="btn"
-          onClick={e => {
-            e.preventDefault();
-            setEdit(false);
-          }}
-        >
+        <button type="submit" className="btn">
           تعديل
         </button>
       </form>
--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Client from "./Client";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Clients = ({ clients }) => {
+  const [formData, setFormData] = useState({});
+  let headersList = {
+    Accept: "/",
+    "Content-Type": "multipart/form-data"
+  };
+  let requestOptions = {
+    url: "https://test.dummydealer.com/api/v1/admin/client",
+    method: "POST",
+    headers: headersList,
+    data: formData
+  };
+  const handleSubmitNew = e => {
+    e.preventDefault();
+    axios
+      .request(requestOptions)
+      .then(() => {
+        document.getElementById("addNewForm").reset();
+        toast.success("تمت الاضافه بنجاح");
+      })
+      .catch(() => {
+        document.getElementById("addNewForm").reset();
+        toast.error("عذرا حدث خطا حاول مره اخرى");
+      });
+  };
   return (
     <main>
       <div className="wrapper">
@@ -9,13 +35,17 @@ const Clients = ({ clients }) => {
           <h2>حذف او تعديل العملاء الحاليين</h2>
           <div className="imgs">
             {clients.map(c => {
-              return <Client client={c} key={c._id} />;
+              return <Client client={c} key={c._id} id={c._id} />;
             })}
           </div>
         </div>
         <div className="add-new">
           <h2>اضافه عميل جديد</h2>
-          <form action="" className="input-box hero-img">
+          <form
+            onSubmit={handleSubmitNew}
+            id="addNewForm"
+            className="input-box hero-img"
+          >
             <div className="file_feild">
               <p>حمل صوره جديده</p>
               <input
@@ -24,13 +54,20 @@ const Clients = ({ clients }) => {
                 name="file"
                 className="file_in"
                 accept="image/*"
+                onChange={e => {
+                  setFormData({ ...formData, image: e.target.files[0] });
+                }}
               />
             </div>
             <textarea
+              required
               placeholder="اضف اسم العميل"
               name="hero-text"
               id="hero-text"
               rows="2"
+              onChange={e => {
+                setFormData({ ...formData, name: e.target.value });
+              }}
             />
             <button type="submit" className="btn">
               اضافه
